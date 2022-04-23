456b49dd79ed6eeacc7341f2d827834041ce57ef
Ezequiel Brunori <e_brunoricarp@hotmail.com>
2022-04-23 11:02:03 -0300

    agregar product details

--- RUNNERS-CODE/app.js
+++ RUNNERS-CODE/app.js
@@ -1,16 +1,22 @@
 let express = require("express");
 const path = require("path");
 let app = express();
-
-const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath))
-
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"))
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
 });
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"))
+});
+
+

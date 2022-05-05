ce5810bd03fab9d74e3905707e7d871a8c0c8e2f
GiselaFux <giselafux@gmail.com>
2022-05-04 23:17:03 -0300

    arreglo de app.set en app.js

--- RUNNERS-CODE/app.js
+++ RUNNERS-CODE/app.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 const mainRouter = require('./src/routers/main')
 app.use('/', mainRouter)
-
+    // view engine setup
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
